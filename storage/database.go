ff42f4e707f8b37fd8d0d3ec75e932cb72fea0b9
gvjacob <gvjacob@outlook.com>
2024-05-12 12:01:45 -0400

    Initialize database

--- storage/database.go
+++ storage/database.go
@@ -2,8 +2,11 @@ package storage
 
 import (
 	"database/sql"
+	"os"
 	"sync"
+	"time"
 
+	"github.com/charmbracelet/log"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -22,6 +25,14 @@ func NewDatabase(file string) (*Database, error) {
     );
   `
 
+	const checkUserQuery = `
+    SELECT COUNT(*) FROM users;
+  `
+
+	const insertUserQuery = `
+    INSERT INTO users (access_token, refresh_token, last_archived) VALUES (?, ?, ?);
+  `
+
 	db, err := sql.Open("sqlite3", file)
 
 	if err != nil {
@@ -30,6 +41,26 @@ func NewDatabase(file string) (*Database, error) {
 
 	if _, err := db.Exec(initQuery); err != nil {
 		return nil, err
+	}
+
+	var count int
+	if err := db.QueryRow(checkUserQuery).Scan(&count); err != nil {
+		return nil, err
+	}
+
+	if count == 0 {
+		log.Warn("No user found, creating a new one")
+
+		_, err := db.Exec(
+			insertUserQuery,
+			os.Getenv("SPOTIFY_ACCESS_TOKEN"),
+			os.Getenv("SPOTIFY_REFRESH_TOKEN"),
+			time.Now().UTC().Format(time.RFC3339),
+		)
+
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return &Database{db: db}, nil
